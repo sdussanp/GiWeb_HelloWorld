8795c3d4e071a241ac6683e87955d2220b69d10a
sdussanp <134747602+sdussanp@users.noreply.github.com>
2024-03-04 15:55:13 -0500

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Senna from "./img/senna.jpg"
+
+function App() {
+  
+  return (
+    <div>
+      <h1>Hola Mundo</h1>
+      <img className= "img-container" src={Senna}/>
+    </div>
+  
+  )
+}
+
+export default App
